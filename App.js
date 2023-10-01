43b929f14024625f81a2d10d6150089f207533db
Sharmila Narendra Babu <86154019+SharmilaNarendra@users.noreply.github.com>
2023-10-01 22:15:09 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-{/* <div id="parent">
-    <div id="child">
-     <h1></h1>
-    </div>
-</div> */}
-
-
+import React from "react"
+import ReactDOM from "react-dom/client";
 
 
 const parent = React.createElement('div',{id:"parent"},
 React.createElement('div',{id:"child"},
 React.createElement('h1',{},"I am a h1 tag"))
 )
-
 
 const heading = React.createElement('h1',{},'Hello Namaste Javascript'); 
 const root = ReactDOM.createRoot(document.getElementById('root'));
